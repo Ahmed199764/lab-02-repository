01ad6ded38df935d195c6b09babc439a7927fd46
Ahmed199764 <Ahmed199764@hotmail.com>
2020-03-31 23:19:28 +0300

    lab2

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # lab-02-repository
 
+### Intro
+This application will provide a gallary service for the users.
+there will be many sections ,depends on which section user will select pictures will be shown.
 
+### Time Mangament
 Number and name of feature | Estimate of time needed to complete | Start time | Finish time | Actual time
 ---------------------------|-------------------------------------|------------|-------------|------------
-Feature #1: Display images | | | |
-Feature #2: Filter images  | | | |
-Feature #3: Style the application | | | |
+Feature #1: Display images | 2 hour | 5pm | 5:30pm | 30min
+Feature #2: Filter images  | 2 hour | 5:30pm | 10pm | 4:30h
+Feature #3: Style the application | 1h | 10pm | 11pm | 1h
 
 
 
--- js/app.js
+++ js/app.js
@@ -1,24 +1,67 @@
-'use strict';
+'use-strict';
 
-$(document).ready(function() {
-  function Gallary(gall) {
-    this.image_url = gall.image_url;
-    this.title = gall.title;
-    this.description = gall.description;
-  }
-  Gallary.prototype.render = function() {
-    let $gallaryClone = $("#photo-template").clone();
-    $gallaryClone.find("h2").text(this.title);
-    $gallaryClone.find("img").attr("src", this.image_url);
-    $gallaryClone.find("p").text(this.description);
-  };
-  const readJson = () => {
-    $.ajax("data/page-1.json", { method: "GET", dataType: "JSON" }).then(data => {
-      data.forEach(gallaryItem => {
-        let gall = new Gallary(gallaryItem);
-        gall.render();
-      });
+
+let myKeywords = ['all'];
+
+function Img(image_url, title, description, keyword, horns) {
+  this.image_url = image_url;
+  this.title = title;
+  this.description = description;
+  this.keyword = keyword;
+  this.horns = horns;
+  Gallary.push(this);
+}
+let Gallary = [];
+
+
+$.get('./data/page-1.json').done(data => {
+  data.forEach(element => {
+    new Img(element.image_url, element.title, element.description, element.keyword, element.horns);
+  });
+  //creates a new section for each image to be more specific in CSS 
+  Gallary.forEach((img) => {
+    let myTemple = $('section');
+    let clone = myTemple.clone();
+
+    clone.attr('id', `${img.keyword}`);
+    clone.attr('class', 'all');
+    clone.children('h2').text(img.title);
+    clone.children('img').attr('src', `${img.image_url}`);
+    clone.children('img').attr('alt', `${img.title}`);
+    clone.children('p').text(img.description);
+    $('main').append(clone[0]);
+    
+    //Inserting each keyword to the list
+    if(!myKeywords.includes(img.keyword)) {
+      myKeywords.push(img.keyword);
+    }
+  });
+
+  //creates a select option for each keyword from the list
+  myKeywords.forEach((keyword) => {
+    let myTemple = $('option');
+    let clone = myTemple.clone();
+    clone.attr('value', keyword);
+    clone.text(keyword);
+    $('select').append(clone[0]);
+  });
+
+  //handles option selection event
+  $('select').change((e) => {
+    $('section').each(function() {
+      $(this).show();
+      // hide the not selected items
+      if( $(this).attr('id') !== e.target.value) {
+        $(this).hide();
+      }
+      // here to show each list output when selected
+      if ( $(this).attr === e.target.value ) {
+        $(this).toggle();
+      }
+      if ( $(this).attr('class') === e.target.value) {
+        $(this).toggle();
+      }
     });
-  };
-  readJson();
+  });
 });
+
